23863039aeb7c8110ae1331d321c35b49b669b3a
lc3 <loren@resonantcircuitlabs.com>
2019-09-19 16:32:20 -0400

    Initial check-in of project structure.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <p>This is now clean and ready to code</p>
+    </div>
+  );
+}
+
+export default App;
