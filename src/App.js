094c69e9d880ed773f84c0dab3dcec593a2ff7a9
lc3 <loren@resonantcircuitlabs.com>
2019-09-22 13:39:14 -0400

    Base react app, no router, no callback

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from 'react-bootstrap/Nav'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <p>This is now clean and ready to code</p>
+    	<Navbar bg="light">
+    		<Navbar.Brand>Reacognito</Navbar.Brand>
+    		<Nav className="mr-auto">
+        </Nav>
+    		<Nav>
+        <Nav.Link href="https://lc3test.auth.us-east-1.amazoncognito.com/login?response_type=code&client_id=1qo2p01r4m9mmf9sbfrkvfir1e&redirect_uri=localhost:3000">Login</Nav.Link>
+    </Nav>
+  		</Navbar>
+      <p>Not Logged In</p>
     </div>
   );
 }
